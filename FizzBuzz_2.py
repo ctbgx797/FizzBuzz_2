3cef1b422c06a75dcf9b869d8f0985a4c87a8d74
Yoshiki <villager2525@gmail.com>
2019-04-17 11:59:58 +0900

    リファクタリング:ガード説を採用

--- FizzBuzz_2.py
+++ FizzBuzz_2.py
@@ -4,18 +4,15 @@ number = int(input("1つの自然数を入れてね: "))
 def fizzbuzz_convert(number):
     if number % 15 == 0:
         return "FizzBuzz"
-    elif number % 3 == 0:
+
+    if number % 3 == 0:
         return "Fizz"
-    elif number % 5 == 0:
+
+    if number % 5 == 0:
         return "Buzz"
-    else:
-        return str(number)
+
+    return str(number)
 
 
 result = fizzbuzz_convert(1)
-print(result)
-
-result = fizzbuzz_convert(3)
-print(result)
-
-fizzbuzz_convert(100)
+print(result)  # 1
